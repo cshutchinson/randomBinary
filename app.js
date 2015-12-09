5e97c611bc8bae3911236f1812c0585be586d09c
Christopher Hutchinson <cshutchinson@gmail.com>
2015-12-09 10:54:24 -0700

    fixed npm module logic by returning funtion

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function randomBinary(){
-  return +(Math.floor(Math.random()*100)).toString(2);
+  return (Math.floor(Math.random()*100)).toString(2);
 }
 
-console.log(randomBinary());
+module.exports = randomBinary;
